f3422e480173e76ab9cb1bdc11862c1910f309a9
Kwonsofia <hyemin0723@swu.ac.kr>
2023-10-24 17:11:00 +0900

    MOD: admin에 wedding_id 조회 기능 추가

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import WeddingMain, Phone, Account, Address, Photos
+from .models import WeddingMain, Phone, Account, Address, Photo
 
 # Register your models here.
-admin.site.register(WeddingMain)
+class WeddingAdmin(admin.ModelAdmin):
+    search_fields = ['wedding_id']
+
+
+admin.site.register(WeddingMain, WeddingAdmin)
 admin.site.register(Phone)
 admin.site.register(Account)
 admin.site.register(Address)
-admin.site.register(Photos)
+admin.site.register(Photo)
