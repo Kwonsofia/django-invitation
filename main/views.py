22899d76f333b9dedbc5e7e53d1dda04b01047be
Kwonsofia <hyemin0723@swu.ac.kr>
2023-10-24 17:32:24 +0900

    MOD: db 조회시 get사용하도록 수정

--- main/views.py
+++ main/views.py
@@ -4,11 +4,10 @@ from .models import WeddingMain, Phone, Account, Photo, Address
 
 # Create your views here.
 def index(request, wedding_id):
-    wedding = WeddingMain.objects.filter(wedding_id=wedding_id)
-    phone = Phone.objects.filter(wedding_id=wedding_id)
-    account = Account.objects.filter(wedding_id=wedding_id)
-    address = Address.objects.filter(wedding_id=wedding_id)
-    print(wedding)
+    wedding = WeddingMain.objects.get(wedding_id=wedding_id)
+    phone = Phone.objects.get(wedding_id=wedding_id)
+    account = Account.objects.get(wedding_id=wedding_id)
+    address = Address.objects.get(wedding_id=wedding_id)
 
     if not wedding:
         return render(request, 'main/error.html')
