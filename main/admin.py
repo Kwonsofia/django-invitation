011ffb2e5e817e3cb7a5c4af9ec3a6b75d5a6249
Kwonsofia <hyemin0723@swu.ac.kr>
2023-10-24 15:39:56 +0900

    ADD: create DB TABLE

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WeddingMain, Phone, Account, Address, Photos
 
 # Register your models here.
+admin.site.register(WeddingMain)
+admin.site.register(Phone)
+admin.site.register(Account)
+admin.site.register(Address)
+admin.site.register(Photos)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+class WeddingMain(models.Model):
+    wedding_id = models.CharField(max_length=20, primary_key=True)
+    passwd = models.CharField(max_length=60)
+    groom_name = models.CharField(max_length=10)
+    groom_father_name = models.CharField(max_length=10, null=True)
+    groom_mother_name = models.CharField(max_length=10, null=True)
+    bride_name = models.CharField(max_length=10)
+    bride_father_name = models.CharField(max_length=10, null=True)
+    bride_mother_name = models.CharField(max_length=10, null=True)
+    use_guestbook = models.BooleanField(default=False)
+    wedding_date = models.DateField("wedding date")
+    wedding_time = models.CharField(max_length=20)
+    reg_dtime = models.DateTimeField(auto_now_add=True)
+
+class Phone(models.Model):
+    wedding_id = models.ForeignKey(WeddingMain, on_delete=models.CASCADE)
+    groom_phone = models.CharField(max_length=13)
+    groom_father_phone = models.CharField(max_length=13, null=True)
+    groom_mother_phone = models.CharField(max_length=13, null=True)
+    bride_phone = models.CharField(max_length=13)
+    bride_father_phone = models.CharField(max_length=13, null=True)
+    bride_mother_phone = models.CharField(max_length=13, null=True)
+
+class Account(models.Model):
+    wedding_id = models.ForeignKey(WeddingMain, on_delete=models.CASCADE)
+    groom_account = models.CharField(max_length=30)
+    groom_father_account = models.CharField(max_length=30, null=True)
+    bride_account = models.CharField(max_length=30)
+    bride_father_account = models.CharField(max_length=30, null=True)
+
+class Address(models.Model):
+    wedding_id = models.ForeignKey(WeddingMain, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    address_tel = models.CharField(max_length=100, null=True)
+
+class GuestBook(models.Model):
+    wedding_id = models.ForeignKey(WeddingMain, on_delete=models.CASCADE)
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10)
+    passwd = models.CharField(max_length=60)
+    message = models.TextField
+    reg_dtime = models.DateTimeField(auto_now_add=True)
+
+class Photos(models.Model):
+    wedding_id = models.ForeignKey(WeddingMain, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='images')  # image를 넣을때는 {id}_{number}.jpg 형식으로 네이밍 필요
