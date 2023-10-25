f5120c5b7a88d49e6709d42ec5ec26bac71b6824
Kwonsofia <hyemin0723@swu.ac.kr>
2023-10-25 09:58:29 +0900

    MOD: 시간필드  추가

--- main/utils.py
+++ main/utils.py
@@ -1,11 +1,12 @@
-def wedding_date(wedding_date):
+def wedding_date(wedding_date, wedding_time):
 
     print(wedding_date)
 
     data = {
         'year': wedding_date.year,
         'month': wedding_date.month,
-        'day': wedding_date.day
+        'day': wedding_date.day,
+        'time': wedding_time
     }
 
     return data
